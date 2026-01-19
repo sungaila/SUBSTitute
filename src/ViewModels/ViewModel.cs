648a4a50fae40d3e7d859df83e79f59c85043e03
David Sungaila <david.sungaila@arcor.de>
2026-01-19 11:26:07 +0100

    Update NuGet packages

--- src/ViewModels/ViewModel.cs
+++ src/ViewModels/ViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Sungaila.SUBSTitute.ViewModels
 {
+    [RequiresUnreferencedCode("Inherits from ObservableValidator which uses RequiresUnreferencedCode.")]
     public partial class ViewModel : ObservableValidator
     {
     }
